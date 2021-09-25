bb27c84da14ba210b1faecc54aaa99b1434635e1
mohamed <mohammed2571994@gmail.com>
2021-09-25 15:58:23 +0300

    Fix a typo

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import {Container, Box, Button} from '@material-ui/core';
-import QList from 'components/Qlist';
-import { useIntl, FormattedMessage } from 'react-intl';
+import QList from 'components/QList';
+import { useIntl } from 'react-intl';
 import { MainLayout } from 'layouts';
-import axios from 'axios';
 import { usePosts } from 'hooks/usePosts';
 
 export default function Home() {
   const { formatMessage } = useIntl();
-  const { data, error, loading } = usePosts({});
+  const { data } = usePosts({});
 
   return (
     <MainLayout>
